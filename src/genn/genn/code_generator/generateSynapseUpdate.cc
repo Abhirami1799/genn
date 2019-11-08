fd35c7f263ac199288a1e8332b0752da4a03090f
neworderofjamie <J.C.Knight@sussex.ac.uk>
2019-11-08 14:36:42 +0000

    Fixed typo in substitutions

--- src/genn/genn/code_generator/generateSynapseUpdate.cc
+++ src/genn/genn/code_generator/generateSynapseUpdate.cc
@@ -118,7 +118,7 @@ void CodeGenerator::generateSynapseUpdate(CodeStream &os, const ModelSpecInterna
 
                 synSubs.addParamValueSubstitution(connectInit.getSnippet()->getParamNames(), connectInit.getParams());
                 synSubs.addParamValueSubstitution(connectInit.getSnippet()->getCombinedDerivedParamNames(), connectInit.getDerivedParams());
-                synSubs.addVarNameSubstitution(connectInit.getSnippet()->getExtraGlobalParams(), "", sg.getName());
+                synSubs.addVarNameSubstitution(connectInit.getSnippet()->getExtraGlobalParams(), "", "", sg.getName());
 
                 std::string pCode = connectInit.getSnippet()->getRowBuildCode();
                 synSubs.applyCheckUnreplaced(pCode, "proceduralSparseConnectivity : " + sg.getName());
